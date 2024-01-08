b4796cc4149804f29f9be64cf3c3bc50de89ce5c
ryuto3384 <ryuto.1128.n@icloud.com>
2024-01-08 14:41:08 +0900

    細かい修正

---
+++ MyCooking/Core/Item/View/ItemHeaderView.swift
@@ -0,0 +1,26 @@
+//
+//  ItemHeaderView.swift
+//  MyCooking
+//
+//  Created by 中島瑠斗 on 2024/01/08.
+//
+
+import SwiftUI
+
+struct ItemHeaderView: View {
+    
+    private let gridItem : [GridItem] = [
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1)
+    ]
+    
+    
+    var body: some View {
+        Text("aaa")
+    }
+}
+
+#Preview {
+    ItemHeaderView()
+}
