4010f0a83cdd0e39c0d870deb5367024ddbd95b1
ryuto3384 <ryuto.1128.n@icloud.com>
2024-01-21 12:33:23 +0900

    細かい修正

---
+++ MyCooking/Core/Upload/View/UploadHashView.swift
@@ -0,0 +1,18 @@
+//
+//  UploadHashView.swift
+//  MyCooking
+//
+//  Created by 中島瑠斗 on 2024/01/21.
+//
+
+import SwiftUI
+
+struct UploadHashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UploadHashView()
+}
