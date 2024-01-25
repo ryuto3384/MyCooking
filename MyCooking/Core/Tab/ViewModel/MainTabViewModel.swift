135c93967a45e4e21ec0807a00efa86daf1c053a
ryuto3384 <ryuto.1128.n@icloud.com>
2024-01-25 16:52:31 +0900

    細かい修正

---
+++ MyCooking/Core/Tab/ViewModel/MainTabViewModel.swift
@@ -0,0 +1,62 @@
+//
+//  MainTabViewModel.swift
+//  MyCooking
+//
+//  Created by 中島瑠斗 on 2024/01/25.
+//
+
+import SwiftUI
+
+class MainTabViewModel: ObservableObject {
+    @Published var posts = [Post]()
+    
+    @Published var displaying_posts: [Post]?
+    
+    @Published var favorite: [Post]?
+    
+    @Published var showProgressFlag: Bool = false
+    
+    @Published var selectCate = ""
+    
+    init(){
+        Task { try await fetchPosts() }
+    }
+    
+    func getIndex(food: Post) -> Int {
+        let index = displaying_posts?.lastIndex(where: { currentFood in
+            return food.id == currentFood.id
+        }) ?? 0
+        
+        return index
+    }
+    
+    func addItem(food: Post) {
+        print(food)
+        if var favorite = self.favorite {
+            if(favorite.count >= 40){
+                favorite.removeLast()
+            }
+            favorite.append(food)
+            self.favorite = favorite
+        } else {
+            self.favorite = [food]
+        }
+        
+    }
+    
+    @MainActor
+    func fetchPosts() async throws {
+        showProgressFlag = true
+        self.posts = try await PostService.fetchFeedPosts(cate: selectCate)
+        self.displaying_posts = self.posts.shuffled()
+        
+        
+        guard displaying_posts != nil else {
+            return
+        }
+//        for post in dispPosts{
+//            print(post.title)
+//        }
+        showProgressFlag = false
+    }
+}
